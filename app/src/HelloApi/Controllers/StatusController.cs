b1ee5a6a190702a8c02d36643bf264a5d4959e8e
andy <andydavies2020@gmail.com>
2019-01-29 21:29:05 +0200

    api: just add an endpoint to return some json

---
+++ app/src/HelloApi/Controllers/StatusController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        private static int _requests;
+
+        [HttpGet]
+        public object Get()
+        {
+            return new {
+                version = GetType().Assembly.GetName().Version.ToString(),
+                host = Environment.MachineName,
+                requestCount = _requests++
+            };
+        }
+    }
+}
--- app/src/HelloApi/Startup.cs
+++ app/src/HelloApi/Startup.cs
@@ -32,15 +32,8 @@ namespace HelloApi
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
 
-            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }

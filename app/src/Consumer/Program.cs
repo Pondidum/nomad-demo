31325c2cc93bc9774012e5e025b25ebc498ad63c
andy <andydavies2020@gmail.com>
2019-03-16 16:41:51 +0200

    apps: add producer and consumer apps

---
+++ app/src/Consumer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using Jobs;
+using MassTransit;
+
+namespace Consumer
+{
+	class Program
+	{
+		private static Random Random = new Random();
+
+		public static async Task Main(string[] args)
+		{
+			var bus = Bus.Factory.CreateUsingRabbitMq(c =>
+			{
+				var host = c.Host(new Uri("rabbitmq://localhost"), r =>
+				{
+					r.Username("guest");
+					r.Password("guest");
+				});
+
+				c.ReceiveEndpoint(host, "jobs", ep => { ep.Handler<Job>(OnJob); });
+			});
+
+			await bus.StartAsync();
+
+			Console.WriteLine("Press any key to exit");
+			Console.ReadKey();
+
+			await bus.StopAsync();
+		}
+
+		private static async Task OnJob(ConsumeContext<Job> context)
+		{
+			var duration = TimeSpan.FromSeconds(Random.Next(0, 5));
+
+			await Task.Delay(duration);
+
+			Console.WriteLine($"Processing {context.Message.Sequence} took {duration.TotalSeconds} seconds");
+		}
+	}
+}
+
+namespace Jobs
+{
+	public class Job
+	{
+		public int Sequence { get; set; }
+	}
+}
---
+++ app/src/Producer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Jobs;
+using MassTransit;
+
+namespace Producer
+{
+	class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var bus = Bus.Factory.CreateUsingRabbitMq(c =>
+			{
+				var host = c.Host(new Uri("rabbitmq://localhost"), r =>
+				{
+					r.Username("guest");
+					r.Password("guest");
+				});
+			});
+
+			await bus.StartAsync();
+
+			var total = args
+				.Where(x => int.TryParse(x, out _))
+				.Select(int.Parse)
+				.DefaultIfEmpty(10)
+				.First();
+
+			Console.WriteLine($"Publishing {total} jobs...");
+
+			for (var i = 0; i < total; i++)
+				await bus.Publish(
+					new Job { Sequence = i },
+					context => context.MessageId = Guid.NewGuid()
+				);
+
+			await bus.StopAsync();
+
+			Console.WriteLine("Done");
+		}
+	}
+}
+
+namespace Jobs
+{
+	public class Job
+	{
+		public int Sequence { get; set; }
+	}
+}

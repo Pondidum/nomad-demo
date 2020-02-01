6a736b0d926cf611142d9df087b96c072a484a6b
andy <andy.davies@reaktor.com>
2020-02-01 11:37:52 +0200

    scripts: use dockerised consul too

--- scripts/host.sh
+++ scripts/host.sh
@@ -2,32 +2,50 @@
 
 set -e
 
+echo "==> Configuring host containers"
+machine_ip=$(ip route get 1 | awk '{print $(NF-2);exit}')
+
+echo "    HostIP: $machine_ip"
+
+echo "    Running Consul"
+docker run -d --rm \
+    --name nomad-consul \
+    --net=host \
+    -e 'CONSUL_LOCAL_CONFIG={"leave_on_terminate": true}' \
+    consul agent -bind="$machine_ip" -retry-join="one.karhu.xyz"
+
+echo "    Running docker registry"
 docker run -d --rm \
     --name nomad-registry \
     -p 5000:5000 \
     registry:latest
 
+echo "    Running http artifacts server"
 docker run -d --rm \
     --name nomad-artifacts \
-    -p 3030:3030 \
-    -e 'PORT=3030' \
-    -v $PWD/app:/web \
+    -p 3000:3000 \
+    -e 'PORT=3000' \
+    -v "$PWD/app:/web" \
     halverneus/static-file-server:latest
 
-echo "pushing consul image to local repository"
+echo "==> Pusing rabbitmq:consul to docker registry"
 docker tag rabbitmq:consul localhost:5000/pondidum/rabbitmq:consul
 docker push localhost:5000/pondidum/rabbitmq:consul
 
-echo "registering artifact server into consul"
+echo "==> Registering services into Consul"
+
+echo "    http artifacts host"
 curl --silent \
     --request PUT \
     --url http://localhost:8500/v1/agent/service/register \
     --header 'content-type: application/json' \
-    --data '{ "ID": "artifacts", "Name": "artifacts", "Port": 3030 }'
+    --data '{ "ID": "artifacts", "Name": "artifacts", "Port": 3000 }'
 
-echo "registering docker registry into consul"
+echo "    docker registry"
 curl --silent \
     --request PUT \
     --url http://localhost:8500/v1/agent/service/register \
     --header 'content-type: application/json' \
     --data '{ "ID": "registry", "Name": "registry", "Port": 5000 }'
+
+echo "==> Done."
